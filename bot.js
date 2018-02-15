6f6c85412195b35834b4dc0908628de1f18a3636
Your Name <you@example.com>
2018-02-15 02:41:28 -0500

    bot initial state active and works

--- bot.js
+++ bot.js
@@ -3,3 +3,35 @@ console.log("bot start");
 var Twit = require("twit");
 var config = require("./config");
 var T = new Twit(config);
+
+// get(), post(), stream()
+
+var stream = T.stream('user');
+stream.on('follow', followed);
+
+function followed(event){
+  console.log(event);
+  var name = event.source.name;
+  var screenName = event.source.screen_name;
+  tweetIt(".@" + screenName + " thank you for the follow! Bot is pleased.")
+}
+
+
+function tweetIt(txt){
+  var r = Math.floor(Math.random()*100);
+  var tweet = {
+    status: txt
+  }
+
+  T.post("statuses/update", tweet, tweeted);
+
+  function tweeted(err, data, response){
+      if (err){
+        console.log("Something went wrong!");
+        console.log(data);
+      } else {
+      console.log("It worked");
+      //console.log(data);
+    }
+  }
+}
--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  consumer_key: "	gUoERFXJikMl1Z8HjOsUDstQF",
-  consumer_secret: "ymdic9UT2844u1Iq0MBQRQZQiE0hcqW8DDgYcYxB3BMf44rE1T",
-  access_token: "	964023416549392384-5H2GDVmRF7quaUuNAzpD8mi5qAfgG9r",
-  access_token_secret: "8vSfNU7LLSbpw5qEIgUZdpUAv4SthJhZNEpXwdlzuFNzK",
+  consumer_key: 'gUoERFXJikMl1Z8HjOsUDstQF',
+  consumer_secret: 'ymdic9UT2844u1Iq0MBQRQZQiE0hcqW8DDgYcYxB3BMf44rE1T',
+  access_token: '964023416549392384-5H2GDVmRF7quaUuNAzpD8mi5qAfgG9r',
+  access_token_secret: '8vSfNU7LLSbpw5qEIgUZdpUAv4SthJhZNEpXwdlzuFNzK',
 
 }
